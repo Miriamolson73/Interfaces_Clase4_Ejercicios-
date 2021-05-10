87328fa3fbcecfb545e4993f9a09d5bbfa75f029
Miriamolson73 <54155622+Miriamolson73@users.noreply.github.com>
2021-05-10 12:04:08 -0300

    ej Dom8.8
    ej Dom8.8

---
+++ ej8.8Dom/ej8.8Dom.js
@@ -0,0 +1,22 @@
+
+var $div = document.querySelector("div");
+
+
+/*  $div.style.background="red";
+$div.style.height="300px";
+$div.style.width="300px";
+*/
+
+//$div.classList.add("square");
+
+
+
+setInterval(function(){$div.classList.toggle("square");},1000);
+  
+
+var swap_1= document.getElementById("source");
+var swap_2= document.getElementById("destination");
+
+
+swap_2.appendChild(swap_1.getElementsByTagName('p')[0]);
+swap_1.appendChild(swap_2.getElementsByTagName('p')[0]);
