f700c94d8ed7abeb9ee8696c52345c7b1cac9dd3
Miriamolson73 <54155622+Miriamolson73@users.noreply.github.com>
2021-05-13 19:57:44 -0300

    commit

--- ej1-2/ej1-2.js
+++ ej1-2/ej1-2.js
@@ -3,11 +3,14 @@
 var body=document.querySelector("#body");
 
 
+
 document.getElementById('button').addEventListener("click", function(){
  
  var parrafoNuevo= document.createElement("p");
  var contenidoParrafo=document.createTextNode("BEEP");
  parrafoNuevo.appendChild(contenidoParrafo);
+
+ //body.insertAdjacentHTML("beforeend",parrafobutton);
  body.appendChild(parrafoNuevo);
 	
  body.classList.toggle("color");
